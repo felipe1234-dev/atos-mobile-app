ac6fc671c56c6ab02cca4a9a5f86127cba6ab754
Felipe Alves <felipejean2002@gmail.com>
2022-06-07 02:06:46 -0300

    feat: Creating a draft of color list for dark and light theme

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+    light: {
+        primary: {
+            10: "#ecfeff",
+            20: "#cffafe",
+            30: "#a5f3fc",
+            40: "#67e8f9",
+            50: "#22d3ee",
+            60: "#06b6d4",
+            70: "#0891b2",
+            80: "#0e7490",
+            90: "#155e75",
+            100: "#164e63"
+        }
+    }
+};
+
+export default colors;
