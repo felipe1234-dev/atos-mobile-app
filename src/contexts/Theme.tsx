478031a081572052f07de54b6b30e1e7f22b52a1
Felipe Alves <felipejean2002@gmail.com>
2022-06-07 02:35:29 -0300

    fix: Preventing theme from being null

--- src/contexts/Theme.tsx
+++ src/contexts/Theme.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 export interface ThemeValue {
-    theme: "light"|"dark"|null,
+    theme: "light"|"dark",
     toggleTheme(): void
 };
 
 export const ThemeContext = React.createContext<ThemeValue>({
-    theme: null,
+    theme: "light",
     toggleTheme: () => {},
 });
 
 export function ThemeProvider(props: { children: React.ReactNode }) {
-    const [theme, setTheme] = React.useState<"light"|"dark"|null>(null);
+    const [theme, setTheme] = React.useState<"light"|"dark">("light");
     
     const toggleTheme = () => setTheme(prevState => (
         prevState === "light" ? "dark" : "light"
