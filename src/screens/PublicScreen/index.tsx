ac90b5d323cc2fd1933cd0346cc45eabe67c73d1
Felipe Alves <felipejean2002@gmail.com>
2022-06-08 21:56:46 -0300

    feat: Creating draft for the unlogged screen

--- src/screens/PublicScreen/index.tsx
+++ src/screens/PublicScreen/index.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { StyleSheet, Image } from "react-native";
-import { Avatar, Box, Text } from "@react-native-material/core";
+import { Box, Text } from "@react-native-material/core";
 
 import DarkThemeLogo from "../../assets/atos-logo-in-dark.png";
-import LightThemeLogo from "../../assets/atos-logo-in-light.png";
 
 import { ThemeContext, ThemeValue } from "../../contexts/Theme";
 import { ScreenProps } from "../../types";
 
 export default function PublicScreen(props: ScreenProps) {
     const { theme, palette } = React.useContext(ThemeContext);
+    const styles = getStyles({ theme, palette });
     
     return (
-        <Box style={styles({ theme, palette }).outerBox}>
-            <Box style={styles({ theme, palette }).innerBox}>
+        <Box style={styles.outerBox}>
+            <Box style={styles.innerBox}>
                 <Image
                     source={{ uri: DarkThemeLogo }}
-                    style={styles({ theme, palette }).logo}
+                    style={styles.logo}
                 />
+                <Text variant="h3" style={styles.title}>
+                    Title
+                </Text>
+                <Text variant="h5" style={styles.subtitle}>
+                    Subtitle
+                </Text>
             </Box>
         </Box>
     );
 };
 
-const styles = ({ palette, theme }: Omit<ThemeValue, "toggleTheme">) => (
+const getStyles = ({ palette, theme }: Omit<ThemeValue, "toggleTheme">) => (
     StyleSheet.create({
         outerBox: {
             display: "flex",
@@ -48,6 +54,14 @@ const styles = ({ palette, theme }: Omit<ThemeValue, "toggleTheme">) => (
             opacity: 1,
             zIndex: 1,
             resizeMode: "contain"
+        },
+        title: {
+            color: palette.primary[0],
+            marginTop: 16
+        },
+        subtitle: {
+            color: palette.primary[50],
+            marginTop: 5
         }
     })
 );
